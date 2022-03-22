5a2277bc343197ebf13cc67f4ec0b45930b5730a
craigharris <harricra@oregonstate.edu>
2022-03-21 21:51:05 -0700

    update var names

--- quick_sort.py
+++ quick_sort.py
@@ -1,15 +1,16 @@
 def quick_sort(arr):
     if len(arr) < 2:
         return arr
+
     pivot_idx = len(arr)//2
-    arr_less, arr_greater = [], []
+    below, above = [], []
     for i in range(len(arr)):
         if i == pivot_idx:
             continue
 
         if arr[i] <= arr[pivot_idx]:
-            arr_less.append(arr[i])
+            below.append(arr[i])
         else:
-            arr_greater.append(arr[i])
+            above.append(arr[i])
 
-    return quick_sort(arr_less) + [arr[pivot_idx]] + quick_sort(arr_greater)
+    return quick_sort(below) + [arr[pivot_idx]] + quick_sort(above)
